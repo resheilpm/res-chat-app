f6d770c50068ae6373bc018d06ee6d2cbe679d50
resheilpm <31864837+resheilpm@users.noreply.github.com>
2017-09-11 16:08:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-	// res.send('<h1>Hello world</h1>');
-	res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
-io.on('connection', function(socket) {
-	console.log('a user connected');
-	socket.on('disconnect', function() {
-		console.log('user disconnected');
-	});
-	socket.on('chat message', function(msg) {
-		io.emit('chat message', msg);
-	});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
-http.listen(3001, function() {
-	console.log('listening on *:3001');
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
