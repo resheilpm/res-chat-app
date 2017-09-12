216e321dbf470bcb9f834f2ff5776d8adad5ec4f
resheilSNT <resheil@stayntouch.com>
2017-09-12 16:11:39 -0400

    modifications

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var express = require('express');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static('./'));
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
